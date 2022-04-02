a42f98bc98b5b564cd6a4d1d2c06c16dc39a53c7
tjdanielson <tessajdanielson@gmail.com>
2022-04-02 13:43:40 -0500

    Created starter html.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        {/* nav bar */}
+        <div> 
+          <h3>DAYTRIP GENERATOR</h3>
+          <div>
+            <img src='./assets/Awaywego.png' alt-text='logo'/>
+          </div>
+        </div>
+        {/* banner */}
+        <div>
+          <div>
+            <img src='./assets/awaywego_white.png' alt-text='logo' />
+          </div>
+          <div>
+            <h2>
+              Welcome to the DAYTRIP GENERATOR
+            </h2>
+            <button>
+              Click here to generate your trip
+            </button>
+          </div>
+        </div>
       </header>
+      <body>
+        {/* conatiner for cards */}
+        <div>
+          {/* card */}
+          <div>
+            <div>
+              <img src='./assets/world.png' />
+              <h3>Location</h3>
+            </div>
+            {/* buttons in card */}
+            <div>
+              <button>
+                Accept
+              </button>
+              <button>
+                New Pick
+              </button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img src='./assets/entertainment.png' />
+              <h3>Entertainment</h3>
+            </div>
+            {/* buttons in card */}
+            <div>
+              <button>
+                Accept
+              </button>
+              <button>
+                New Pick
+              </button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img src='./assets/restaurant.png' />
+              <h3>Restaurant</h3>
+            </div>
+            {/* buttons in card */}
+            <div>
+              <button>
+                Accept
+              </button>
+              <button>
+                New Pick
+              </button>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img src='./assets/transportation.png' />
+              <h3>Transportation</h3>
+            </div>
+            {/* buttons in card */}
+            <div>
+              <button>
+                Accept
+              </button>
+              <button>
+                New Pick
+              </button>
+            </div>
+          </div>
+        </div>
+        <img src='./assets/awaywego_white.png' />
+        <button>
+          Click here to start over
+        </button>
+      </body>
     </div>
   );
 }

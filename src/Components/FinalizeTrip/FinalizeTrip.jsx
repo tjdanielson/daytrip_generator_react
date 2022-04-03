9c5759248a0f73286edf2ef965d940ffb7ee67e2
tjdanielson <tessajdanielson@gmail.com>
2022-04-02 21:06:07 -0500

    Add styling for finalize trip button and restart button.

--- src/Components/FinalizeTrip/FinalizeTrip.jsx
+++ src/Components/FinalizeTrip/FinalizeTrip.jsx
@@ -9,33 +9,33 @@ const FinalizeTrip = (props) => {
     function PrintResults() {
         return (
             <div>
-            <h2>Itinerary</h2>
-            <div>
-                <h3>Item one</h3>
-                <p>You will be traveling to...</p>
-                <img src='' />
-            </div>
-            <div>
-                <h3>Item two</h3>
-                <p>While in place, you will be attending a(n)...</p>
-                <img src='' />
-            </div>
-            <div>
-                <h3>Item three</h3>
-                <p>After your entertainment, you will enjoy a meal at restaurant</p>
-                <img src='' />
-            </div>
-            <div>
-                <h3>Item four</h3>
-                <p>Over the course of your day trip, you will be traveling by ...</p>
-                <img src='' />
-            </div>
+                <h2>Itinerary</h2>
+                <div>
+                    <h3>Item one</h3>
+                    <p>You will be traveling to...</p>
+                    <img src='' />
+                </div>
+                <div>
+                    <h3>Item two</h3>
+                    <p>While in place, you will be attending a(n)...</p>
+                    <img src='' />
+                </div>
+                <div>
+                    <h3>Item three</h3>
+                    <p>After your entertainment, you will enjoy a meal at restaurant</p>
+                    <img src='' />
+                </div>
+                <div>
+                    <h3>Item four</h3>
+                    <p>Over the course of your day trip, you will be traveling by ...</p>
+                    <img src='' />
+                </div>
         </div>
-        )
+        );
     }
     
     return (
-        <div>
+        <div className='finalize-trip-container'>
             <button onClick={onClick} style={{cursor: 'pointer'}}>Finalize your trip!</button>
             {showResults ? <PrintResults /> : null}
         </div>
